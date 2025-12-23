02288293c8708b1ea653f0436077db7ed5cfecba
6838gamal <89938004+6838gamal@users.noreply.github.com>
2025-12-23 15:55:13 +0300

    Refactor profile app bar widget styling
    Updated the profile app bar widget's padding and colors.

--- lib/page/profile_page/widget/profile_app_bar_widget.dart
+++ lib/page/profile_page/widget/profile_app_bar_widget.dart
@@ -11,19 +11,27 @@ class ProfileAppBarWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: MediaQuery.of(context).viewPadding.top + 50,
-      padding: EdgeInsets.only(top: MediaQuery.of(context).viewPadding.top, left: 15, right: 15),
+      padding: EdgeInsets.only(
+        top: MediaQuery.of(context).viewPadding.top,
+        left: 15,
+        right: 15,
+      ),
       alignment: Alignment.center,
       width: Get.width,
-      color: AppColor.transparent,
+      color: const Color(0xFF4E342E), // بني فاخر
       child: Row(
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
           Text(
             EnumLocal.txtMyProfile.name.tr,
-            style: AppFontStyle.styleW700(AppColor.black, 20),
+            style: AppFontStyle.styleW700(
+              const Color(0xFFFFD54F), // نص ذهبي
+              20,
+            ),
           ),
         ],
       ),
     );
   }
 }
+
